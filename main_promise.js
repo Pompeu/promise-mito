7c19b6a155c0f10767192c91c620f063ce06dbd2
Pompeu <itacir@hotmail.com>
2015-10-13 12:20:10 -0300

    main with promise done

---
+++ main_promise.js
@@ -0,0 +1,19 @@
+'use strict';
+const banheiro = require('./banheiro');
+const deffer = require('./deffer');
+let promises = [];
+
+let abrir_porta = deffer(banheiro.abrir_porta);
+let levantar_tampa =  deffer(banheiro.levantar_tampa);
+let acender_luz = deffer(banheiro.acender_luz);
+let tirando_fazedor_de_xixi = deffer(banheiro.tirando_fazedor_de_xixi);
+
+promises.push(abrir_porta(true));
+promises.push(levantar_tampa(true));
+promises.push(acender_luz(true));
+promises.push(tirando_fazedor_de_xixi(true));
+
+Promise
+	.all(promises)
+	.then(console.log,console.error)
+
